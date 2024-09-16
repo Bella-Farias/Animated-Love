f9d02097fdb891b1007f0a359b20c6514c65623a
Isabella Farias <belllafarias99@gmail.com>
2024-09-16 17:49:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const boxes = document.querySelectorAll(".box");
 
 window.addEventListener("scroll", checkBoxes);
-window.addEventListener("touchmove", checkBoxes);
+window.addEventListener("resize", checkBoxes);
 
 checkBoxes();
 
 function checkBoxes(){
     const triggerBtn = window.innerHeight / 5 * 4.3;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < triggerBtn){
+            box.classList.add("show");
+        }else{
+            box.classList.remove("show");
+        }
+    })
+}
+
+
+function checkBoxes(){
+    const triggerBtn = window.innerHeight / 1.2;
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
